a96ce4d158fd0aaa2bf790b42dde9495b39760da
lukasz <lukasz@student.edu>
2017-05-12 11:56:32 +0200

    add getters and setters to entity Offer and User

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -8,6 +8,7 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -24,9 +25,49 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    /**
+     * @var
+     * @ORM\OneToMany(targetEntity="AppBundle\Entity\Offer", mappedBy="user")
+     *
+     */
+    private $offers;
 
     public function __construct()
     {
         parent::__construct();
+        $this->offers = new ArrayCollection();
+    }
+
+    /**
+     * Add offers
+     *
+     * @param \AppBundle\Entity\Offer $offers
+     * @return User
+     */
+    public function addOffer(\AppBundle\Entity\Offer $offers)
+    {
+        $this->offers[] = $offers;
+
+        return $this;
+    }
+
+    /**
+     * Remove offers
+     *
+     * @param \AppBundle\Entity\Offer $offers
+     */
+    public function removeOffer(\AppBundle\Entity\Offer $offers)
+    {
+        $this->offers->removeElement($offers);
+    }
+
+    /**
+     * Get offers
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getOffers()
+    {
+        return $this->offers;
     }
 }
