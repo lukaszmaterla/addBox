c05d7111ea023abbab631041595197dcbcf723e3
lukasz <lukasz@student.edu>
2017-05-25 15:19:59 +0200

    users can change additional properties in profile edit page, to register only username password and email is needed

---
+++ src/AppBundle/Form/ProfileType.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lukasz
+ * Date: 25.05.17
+ * Time: 14:34
+ */
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('city')
+            ->add('phoneNumber');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\ProfileFormType';
+    }
+    public function getBlockPrefix()
+    {
+        return 'app_user_profile';
+    }
+
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+}
